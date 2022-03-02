d4cf4dea3afef4816b70a253be10580f2548a5a4
Albin Johansson <albinoscar99@hotmail.com>
2022-03-03 00:12:25 +0100

    Update doxygen.rs

--- src/d2m/doxygen.rs
+++ src/d2m/doxygen.rs
@@ -1,8 +1,55 @@
-use std::borrow::BorrowMut;
+use AccessModifier::*;
+
 use std::collections::HashMap;
 use std::collections::hash_map::Iter;
 
 pub type RefID = String;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum AccessModifier {
+    PRIVATE,
+    PROTECTED,
+    PUBLIC,
+}
+
+#[derive(Debug)]
+pub struct Function {
+    pub name: String,
+    pub qualified_name: String,
+    pub return_type: String,
+    pub template_parameters: String,
+    pub arguments: String,
+    pub definition: String,
+    pub access: AccessModifier,
+    pub is_static: bool,
+    pub is_const: bool,
+    pub is_inline: bool,
+    pub is_noexcept: bool,
+    pub is_virtual: bool,
+    pub is_explicit: bool,
+    pub is_member: bool,
+}
+
+impl Function {
+    pub fn new() -> Self {
+        Self {
+            name: String::from("?"),
+            qualified_name: String::from("?"),
+            return_type: String::from("?"),
+            template_parameters: String::from("?"),
+            arguments: String::from("?"),
+            definition: String::from("?"),
+            access: PRIVATE,
+            is_static: false,
+            is_const: false,
+            is_inline: false,
+            is_noexcept: false,
+            is_virtual: false,
+            is_explicit: false,
+            is_member: false,
+        }
+    }
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum CompoundKind {
@@ -24,11 +71,6 @@ pub struct Compound {
     pub kind: CompoundKind,
 }
 
-#[derive(Debug)]
-pub struct Registry {
-    compounds: HashMap<RefID, Compound>,
-}
-
 impl Compound {
     pub fn new() -> Self {
         Self {
@@ -37,6 +79,11 @@ impl Compound {
             kind: CompoundKind::UNKNOWN,
         }
     }
+}
+
+#[derive(Debug)]
+pub struct Registry {
+    compounds: HashMap<RefID, Compound>,
 }
 
 impl Registry {
