60469dbe5a88859a47ada3fafa06b805cb214e9d
Albin Johansson <albinoscar99@hotmail.com>
2022-04-01 16:40:16 +0200

    Use BufWriter in generator

--- src/d2m/generator.rs
+++ src/d2m/generator.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Write};
+use std::io::{self, BufWriter, Write};
 use std::path::PathBuf;
 
 use crate::d2m::doxygen::{Compound, Function, RefID, Registry};
@@ -26,123 +26,151 @@ fn generate_index_file(output_dir: &PathBuf, registry: &Registry) -> io::Result<
   let path = output_dir.join("index.md");
   println!("Generating file {}", path.display());
 
-  let mut file = File::create(path)?;
+  let file = File::create(path)?;
+  let mut writer = BufWriter::new(&file);
 
-  write!(file, "# API\n")?;
-  write!(file, "\nHere is a list of all modules.\n")?;
+  write!(writer, "# API\n")?;
+  write!(writer, "\nHere is a list of all modules.\n")?;
 
-  write!(file, "\n## Modules\n\n")?;
+  write!(writer, "\n## Modules\n\n")?;
 
   for (_, compound) in &registry.compounds {
     // TODO arrange by group relations (subgroups)
     if compound.kind == GROUP {
-      write!(file, "* [{}](groups/{})\n", &compound.title, generate_group_filename(&compound.name))?;
+      write!(writer, "* [{}](groups/{})\n", &compound.title, generate_group_filename(&compound.name))?;
     }
   }
 
   Ok(())
 }
 
-fn generate_template_parameter_docs(file: &mut File, parameters: &HashMap<String, String>)
+fn generate_template_parameter_docs(writer: &mut BufWriter<&File>, parameters: &HashMap<String, String>)
   -> io::Result<()>
 {
   if !parameters.is_empty() {
-    write!(file, "\n**Template Parameters**\n\n")?;
+    write!(writer, "\n**Template Parameters**\n\n")?;
 
     for (name, info) in parameters.iter() {
-      write!(file, "* `{}` {}\n", name, if info.is_empty() { "N/A" } else { info })?;
+      write!(writer, "* `{}` {}\n", name, if info.is_empty() { "N/A" } else { info })?;
     }
 
-    write!(file, "\n")?;
+    write!(writer, "\n")?;
   }
 
   Ok(())
 }
 
-fn generate_function_definition(file: &mut File, func: &Function) -> io::Result<()>
+fn generate_function_definition(writer: &mut BufWriter<&File>, func: &Function) -> io::Result<()>
 {
-  write!(file, "\n---\n")?;
-  write!(file, "\n### **{}**\n", &func.qualified_name)?;
+  write!(writer, "\n---\n")?;
+  write!(writer, "\n### **{}**\n", &func.qualified_name)?;
 
   if !func.docs.brief.is_empty() {
     for docs in &func.docs.brief {
-      write!(file, "\n**Synopsis**: {}\n", docs)?;
+      write!(writer, "\n**Synopsis**: {}\n", docs)?;
     }
   } else {
-    write!(file, "\nThis function has no brief documentation.\n")?;
+    write!(writer, "\nThis function has no brief documentation.\n")?;
   }
 
   if !func.docs.details.is_empty() {
     for docs in &func.docs.details {
-      write!(file, "\n{}\n", docs)?;
-    }
-  }
-
-  if !func.docs.notes.is_empty() {
-    for note in &func.docs.notes {
-      write!(file, "\n!!! note\n")?;
-      write!(file, "    {}\n", note)?;
+      write!(writer, "\n{}\n", docs)?;
     }
   }
 
   if func.is_member {
-    write!(file, "\n*This is a {} function.*\n", func.access)?;
+    write!(writer, "\n*This is a {} function.*\n", func.access)?;
   }
 
-  generate_template_parameter_docs(file, &func.docs.template_parameters)?;
+  if !func.docs.notes.is_empty() {
+    for note in &func.docs.notes {
+      write!(writer, "\n!!! note\n")?;
+      write!(writer, "    {}\n", note)?;
+    }
+  }
+
+  if !func.docs.warnings.is_empty() {
+    for warning in &func.docs.warnings {
+      write!(writer, "\n!!! warning\n")?;
+      write!(writer, "    {}\n", warning)?;
+    }
+  }
+
+  if !func.docs.pre_conditions.is_empty() {
+    write!(writer, "\n**Pre-conditions**\n\n")?;
+
+    for cond in &func.docs.pre_conditions {
+      write!(writer, "* {}\n", cond)?;
+    }
+
+    write!(writer, "\n")?;
+  }
+
+  if !func.docs.post_conditions.is_empty() {
+    write!(writer, "\n**Post-conditions**\n\n")?;
+
+    for cond in &func.docs.post_conditions {
+      write!(writer, "* {}\n", cond)?;
+    }
+
+    write!(writer, "\n")?;
+  }
+
+  generate_template_parameter_docs(writer, &func.docs.template_parameters)?;
 
   if !func.parameter_names.is_empty() {
-    write!(file, "\n**Parameters**\n\n")?;
+    write!(writer, "\n**Parameters**\n\n")?;
 
     for name in &func.parameter_names {
-      write!(file, "* `{}`", name)?;
+      write!(writer, "* `{}`", name)?;
 
       match func.docs.parameters.get(name) {
-        Some(desc) => write!(file, " {}", desc)?,
-        None => write!(file, " N/A")?
+        Some(desc) => write!(writer, " {}", desc)?,
+        None => write!(writer, " N/A")?
       }
 
-      write!(file, "\n")?;
+      write!(writer, "\n")?;
     }
   }
 
   if !func.docs.exceptions.is_empty() {
-    write!(file, "\n**Exceptions**\n\n")?;
+    write!(writer, "\n**Exceptions**\n\n")?;
 
     for (name, desc) in &func.docs.exceptions {
-      write!(file, "* `{}` {}\n", name, desc)?;
+      write!(writer, "* `{}` {}\n", name, desc)?;
     }
 
-    write!(file, "\n")?;
+    write!(writer, "\n")?;
   }
 
   if !func.docs.returns.is_empty() {
-    write!(file, "\n**Returns**\n\n")?;
-    write!(file, "{}\n", &func.docs.returns)?;
+    write!(writer, "\n**Returns**\n\n")?;
+    write!(writer, "{}\n", &func.docs.returns)?;
   }
 
-  write!(file, "\n```C++\n")?;
+  write!(writer, "\n```C++\n")?;
   if !func.template_args.is_empty() {
-    write!(file, "template <")?;
+    write!(writer, "template <")?;
     let mut first = true;
     for arg in &func.template_args {
-      write!(file, "{}{}", if !first { ", " } else { "" }, arg)?;
+      write!(writer, "{}{}", if !first { ", " } else { "" }, arg)?;
       first = false;
     }
-    write!(file, ">\n")?;
+    write!(writer, ">\n")?;
   }
-  write!(file, "{}{}{} {}{};\n",
+  write!(writer, "{}{}{} {}{};\n",
          if func.is_static { "static " } else { "" },
          if func.is_explicit { "explicit " } else { "" },
          &func.return_type,
          &func.name,
          &func.args)?;
-  write!(file, "```\n")?;
+  write!(writer, "```\n")?;
 
   if !func.docs.see_also.is_empty() {
-    write!(file, "\n**See Also**\n\n")?;
+    write!(writer, "\n**See Also**\n\n")?;
     for see in &func.docs.see_also {
-      write!(file, "* {}\n", see)?;
+      write!(writer, "* {}\n", see)?;
     }
   }
 
@@ -158,40 +186,39 @@ fn generate_class_file(destination: &PathBuf,
 
   let class = registry.classes.get(compound_id).unwrap();
 
-  let file = File::create(destination);
-  if let Ok(mut file) = file {
-    write!(file, "# {}\n", &compound.name)?;
+  let file = File::create(destination)?;
+  let mut writer = BufWriter::new(&file);
+  write!(writer, "# {}\n", &compound.name)?;
 
-    write!(file, "\n```C++\n")?;
-    if !class.template_args.is_empty() {
-      write!(file, "template <")?;
-      for arg in &class.template_args {
-        write!(file, "{}", arg)?;
-      }
-      write!(file, ">\n")?;
+  write!(writer, "\n```C++\n")?;
+  if !class.template_args.is_empty() {
+    write!(writer, "template <")?;
+    for arg in &class.template_args {
+      write!(writer, "{}", arg)?;
     }
-    write!(file,
-           "{} {};\n",
-           if class.is_struct { "struct" } else { "class" },
-           &class.unqualified_name)?;
-    write!(file, "```\n")?;
+    write!(writer, ">\n")?;
+  }
+  write!(writer,
+         "{} {};\n",
+         if class.is_struct { "struct" } else { "class" },
+         &class.unqualified_name)?;
+  write!(writer, "```\n")?;
 
-    for par in &compound.brief_docs {
-      write!(file, "\n{}\n", par)?;
-    }
+  for par in &compound.brief_docs {
+    write!(writer, "\n{}\n", par)?;
+  }
 
-    write!(file, "\n## Detailed Description\n\n")?;
+  write!(writer, "\n## Detailed Description\n\n")?;
 
-    for par in &compound.detailed_docs {
-      write!(file, "\n{}\n", par)?;
-    }
+  for par in &compound.detailed_docs {
+    write!(writer, "\n{}\n", par)?;
+  }
 
-    write!(file, "\n## Members\n")?;
+  write!(writer, "\n## Members\n")?;
 
-    for func_id in &compound.functions {
-      let func = registry.functions.get(func_id).unwrap();
-      generate_function_definition(&mut file, &func)?;
-    }
+  for func_id in &compound.functions {
+    let func = registry.functions.get(func_id).unwrap();
+    generate_function_definition(&mut writer, &func)?;
   }
 
   Ok(())
@@ -201,76 +228,92 @@ fn generate_group_file(destination: &PathBuf,
                        registry: &Registry,
                        compound: &Compound) -> io::Result<()>
 {
-  // println!("Generating file {}", destination.display());
-  let file = File::create(destination);
-  if let Ok(mut file) = file {
-    write!(file, "# {}\n", &compound.title)?;
+  println!("Generating file {}", destination.display());
 
-    for par in &compound.brief_docs {
-      write!(file, "\n{}\n", &par)?;
+  let file = File::create(destination)?;
+  let mut writer = BufWriter::new(&file);
+
+  write!(writer, "# {}\n", &compound.title)?;
+
+  for par in &compound.brief_docs {
+    write!(writer, "\n{}\n", &par)?;
+  }
+
+  for par in &compound.detailed_docs {
+    write!(writer, "\n{}\n", &par)?;
+  }
+
+  write!(writer, "\n## Groups\n\n")?;
+  if compound.groups.is_empty() {
+    write!(writer, "There are no associated subgroups for this group.\n")?;
+  } else {
+    for group_id in &compound.groups {
+      let group = registry.compounds.get(group_id).unwrap();
+      write!(writer, "* {}\n", &group.title)?;
     }
+  }
 
-    for par in &compound.detailed_docs {
-      write!(file, "\n{}\n", &par)?;
+  write!(writer, "\n## Classes & Structs\n\n")?;
+  if compound.classes.is_empty() {
+    write!(writer, "There are no classes or structs associated with this group.\n")?;
+  } else {
+    for class_id in &compound.classes {
+      let class = registry.classes.get(class_id).unwrap();
+      let class_compound = registry.compounds.get(class_id).unwrap();
+      let filename = get_class_filename(&class_compound.name);
+      write!(writer,
+             "* [{} {}](../classes/{})\n",
+             if class.is_struct { "struct" } else { "class" },
+             &class.unqualified_name,
+             &filename)?;
     }
+  }
 
-    write!(file, "\n## Groups\n\n")?;
-    if compound.groups.is_empty() {
-      write!(file, "There are no associated subgroups for this group.\n")?;
-    } else {
-      for group_id in &compound.groups {
-        let group = registry.compounds.get(group_id).unwrap();
-        write!(file, "* {}\n", &group.title)?;
+  write!(writer, "\n## Functions\n\n")?;
+  if compound.functions.is_empty() {
+    write!(writer, "There are no functions associated with this group.\n")?;
+  } else {
+    write!(writer, "These are the free functions associated with this group.\n")?;
+    for func_id in &compound.functions {
+      let func = registry.functions.get(func_id).unwrap();
+      if !func.is_member {
+        generate_function_definition(&mut writer, func)?;
       }
     }
+  }
 
-    write!(file, "\n## Classes & Structs\n\n")?;
-    if compound.classes.is_empty() {
-      write!(file, "There are no classes or structs associated with this group.\n")?;
-    } else {
-      for class_id in &compound.classes {
-        let class = registry.classes.get(class_id).unwrap();
-        let class_compound = registry.compounds.get(class_id).unwrap();
-        let filename = get_class_filename(&class_compound.name);
-        write!(file,
-               "* [{} {}](../classes/{})\n",
-               if class.is_struct { "struct" } else { "class" },
-               &class.unqualified_name,
-               &filename)?;
-      }
-    }
+  write!(writer, "\n## Variables\n\n")?;
+  if compound.variables.is_empty() {
+    write!(writer, "There are no variables associated with this group.\n")?;
+  } else {
+    write!(writer, "These are the variables associated with this group.\n")?;
 
-    write!(file, "\n## Functions\n\n")?;
-    if compound.functions.is_empty() {
-      write!(file, "There are no functions associated with this group.\n")?;
-    } else {
-      write!(file, "These are the free functions associated with this group.\n")?;
-      for func_id in &compound.functions {
-        let func = registry.functions.get(func_id).unwrap();
-        if !func.is_member {
-          generate_function_definition(&mut file, func)?;
+    for variable_id in &compound.variables {
+      let variable = registry.variables.get(variable_id).unwrap();
+
+      write!(writer, "\n### {}\n", &variable.qualified_name)?;
+
+      if !variable.docs.brief.is_empty() {
+        for brief in &variable.docs.brief {
+          write!(writer, "\n{}\n", brief)?;
         }
       }
-    }
 
-    write!(file, "\n## Variables\n\n")?;
-    if compound.variables.is_empty() {
-      write!(file, "There are no variables associated with this group.\n")?;
-    } else {
-      for variable_id in &compound.variables {
-        let variable = registry.variables.get(variable_id).unwrap();
-        write!(file, "* {}\n", &variable.name)?;
-      }
-    }
+      write!(writer, "```C++\n")?;
+      write!(writer, "{};\n", &variable.definition)?;
+      write!(writer, "```\n")?;
 
-    write!(file, "\n## Defines\n\n")?;
-    if compound.defines.is_empty() {
-      write!(file, "There are no defines associated with this group.\n")?;
-    } else {
-      for define_id in &compound.defines {
-        let define = registry.defines.get(define_id).unwrap();
-        write!(file, "* {}\n", &define.name)?;
-      }
+      write!(writer, "\n---\n")?;
+    }
+  }
+
+  write!(writer, "\n## Defines\n\n")?;
+  if compound.defines.is_empty() {
+    write!(writer, "There are no defines associated with this group.\n")?;
+  } else {
+    for define_id in &compound.defines {
+      let define = registry.defines.get(define_id).unwrap();
+      write!(writer, "* {}\n", &define.name)?;
     }
   }
 
